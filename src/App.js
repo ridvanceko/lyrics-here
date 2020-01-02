78e3e9c9dda13e53833d139e47b0258af59e9f08
Ridvan Cekmez <ridvanceko@gmail.com>
2020-01-01 21:13:13 -0700

    tracks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import  { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Navbar from './components/layouts/Navbar'
-import Index from './components/layouts/Index'
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Index from "./components/layouts/Index";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-            <Navbar/>
-    </div>
-    <div className="container">
-      <Switch>
-        <Route exact path="/" component={index}/>
-      </Switch>
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
 
-    </div>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={index} />
+        </Switch>
+      </div>
     </Router>
   );
 }
